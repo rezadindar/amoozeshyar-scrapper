f2a5cb74572effc323acb3570f871012eac65b08
MRDindar <rezadindar1381@gmail.com>
2023-04-17 18:38:19 +0330

    Update amoozeshyar.py

--- amoozeshyar.py
+++ amoozeshyar.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait 
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
