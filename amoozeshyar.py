49fad0a62d73ce41c49be8afe14316f0461945d0
MRDindar <rezadindar1381@gmail.com>
2023-10-01 20:32:52 +0330

    Improve Error Handling

--- amoozeshyar.py
+++ amoozeshyar.py
@@ -5,9 +5,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
-import requests, base64, time, os, ntpath, glob, shutil
+import requests
+import base64
+import time
+import os
+import ntpath
+import glob
+import shutil
 from fastapi import FastAPI
-import multiprocessing
+from multiprocessing import Pipe
+from multiprocessing import Process
 import sqlite3
 from fastapi.responses import FileResponse
 from retry import retry
@@ -19,24 +26,26 @@ config = db.execute("SELECT * from config").fetchone()
 db.commit()
 db.close()
 
-PATH =  os.path.join(os.getcwd(), 'files')
+PATH = os.path.join(os.getcwd(), 'files')
 
 if not os.path.exists("files"):
     os.makedirs("files")
 
-def solve(f):
-	with open(f, "rb") as image_file:
-		encoded_string = base64.b64encode(image_file.read()).decode('ascii')
-		url = 'https://api.apitruecaptcha.org/one/gettext'
 
-		data = { 
+def solve(f):
+    with open(f, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('ascii')
+        url = 'https://api.apitruecaptcha.org/one/gettext'
+
+        data = {
             'userid': config[5],
-			'apikey': config[6],
-			'data':encoded_string
-		}
-		response = requests.post(url = url, json = data)
-		data = response.json()
-		return data
+            'apikey': config[6],
+            'data': encoded_string
+        }
+        response = requests.post(url=url, json=data)
+        data = response.json()
+        return data
+
 
 def cleanDir(folder):
     for filename in os.listdir(folder):
@@ -49,23 +58,28 @@ def cleanDir(folder):
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+
 def checkIsCsv(filename):
     if filename.endswith(".csv"):
         return True
     else:
         return False
 
+
 app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup_event():
     db = sqlite3.connect('amoozeshyar.db')
     cursor = db.cursor()
-    cursor.execute("UPDATE process_status SET fetchStatus = 'False' WHERE id = 1")
+    cursor.execute(
+        "UPDATE process_status SET fetchStatus = 'False', fetchError = 'False' WHERE id = 1")
     db.commit()
     db.close()
 
     cleanDir("files")
+
 
 @app.get("/fetch")
 def fetch():
@@ -74,13 +88,19 @@ def fetch():
 
     if processStatus[1] == 'False':
         cursor = db.cursor()
-        cursor.execute("UPDATE process_status SET fetchStatus = 'fetching' WHERE id = 1")
+        cursor.execute(
+            "UPDATE process_status SET fetchError = 'False', fetchStatus = 'fetching' WHERE id = 1")
         db.commit()
         db.close()
 
         cleanDir("files")
 
-        process = multiprocessing.Process(target=runFetch)
+        # create the pipe
+        conn1, conn2 = Pipe()
+        # create a child process process
+        process = Process(target=runFetch, args=(conn2,))
+        process.start()
+        process = Process(target=runFetchResult, args=(conn1,))
         process.start()
 
         result = {
@@ -93,33 +113,57 @@ def fetch():
 
     return result
 
+
+def registerFetchError():
+    db = sqlite3.connect('amoozeshyar.db')
+    cursor = db.cursor()
+    cursor.execute(
+        "UPDATE process_status SET fetchStatus = 'False', fetchError = 'True' WHERE id = 1")
+    db.commit()
+    db.close()
+
+
+def runFetchResult(conn1):
+    try:
+        msg = conn1.recv()
+        if msg == 'Error':
+            print('Ended With Error!')
+            registerFetchError()
+    except:
+        registerFetchError()
+        print('Ended With Error!')
+
+
 @retry(Exception, tries=3, delay=3)
-def runFetch():
+def runFetch(connection):
     chrome_options = Options()
     prefs = {
-        "download.default_directory" : PATH,
-        #"download.open_pdf_in_system_reader": False,
-        #"download.prompt_for_download": True,
-        #"download_restrictions": 3,
-        #"plugins.always_open_pdf_externally": False,
-        #"plugins.plugins_disabled" : ["Chrome PDF Viewer"]
+        "download.default_directory": PATH,
+        # "download.open_pdf_in_system_reader": False,
+        # "download.prompt_for_download": True,
+        # "download_restrictions": 3,
+        # "plugins.always_open_pdf_externally": False,
+        # "plugins.plugins_disabled" : ["Chrome PDF Viewer"]
     }
     chrome_options.add_argument('--headless=new')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument(
+        "--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.page_load_strategy = 'eager'
     chrome_options.add_experimental_option("prefs", prefs)
-    browser = webdriver.Chrome(ChromeDriverManager(version=config[7]).install(), chrome_options=chrome_options)
+    browser = webdriver.Chrome(ChromeDriverManager(
+        version=config[7]).install(), chrome_options=chrome_options)
     actions = ActionChains(browser)
 
     browser.get(config[1])
     assert 'ورود به سيستم' in browser.title
 
     try:
-        elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, "B1")))
+        elemReady = WebDriverWait(browser, 30).until(
+            EC.presence_of_element_located((By.NAME, "B1")))
         print("Login Page is ready!")
     except TimeoutException:
         print("Loading took too much time!")
@@ -135,7 +179,14 @@ def runFetch():
     with open('captcha.png', 'wb') as file:
         file.write(captchaImage.screenshot_as_png)
 
-    captchaText = solve('captcha.png')['result']
+    sovleCaptcha = solve('captcha.png')
+    try:
+        captchaText = sovleCaptcha['result']
+    except:
+        if sovleCaptcha['error_type'] == 'QueryException':
+            print('Captcha Credit Is Over!')
+            connection.send('Error')
+            return False
 
     captchaFiled = browser.find_element(By.NAME, 'jcaptcha')
     captchaFiled.send_keys(captchaText)
@@ -144,7 +195,8 @@ def runFetch():
     actions.click(loginButton).perform()
 
     try:
-        elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "logintime")))
+        elemReady = WebDriverWait(browser, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "logintime")))
         print("After Login Page is ready!")
     except TimeoutException:
         print("Loading took too much time!")
@@ -152,12 +204,14 @@ def runFetch():
     browser.switch_to.frame(browser.find_elements(By.TAG_NAME, "iframe")[0])
 
     try:
-        elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "l1")))
+        elemReady = WebDriverWait(browser, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "l1")))
         print("Main Menu is ready!")
     except TimeoutException:
         print("Loading took too much time!")
 
-    barnameRiziMenu = browser.find_element(By.XPATH, "//*[contains(text(), 'برنامه ريزي آموزشي نيمسال تحصيلي')]")
+    barnameRiziMenu = browser.find_element(
+        By.XPATH, "//*[contains(text(), 'برنامه ريزي آموزشي نيمسال تحصيلي')]")
     actions.click(barnameRiziMenu).perform()
 
     time.sleep(1)
@@ -166,43 +220,50 @@ def runFetch():
         if os.path.exists(os.path.join(PATH, str(i) + '.csv')):
             continue
 
-        klasseDarsHa = browser.find_element(By.XPATH, "//*[contains(text(), 'كلاس درسها')]")
+        klasseDarsHa = browser.find_element(
+            By.XPATH, "//*[contains(text(), 'كلاس درسها')]")
         actions.click(klasseDarsHa).perform()
 
         window_after = browser.window_handles[1]
         browser.switch_to.window(window_after)
 
         try:
-            elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "reporttitle")))
+            elemReady = WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "reporttitle")))
             print("Report Page is ready!")
         except TimeoutException:
             print("Loading took too much time!")
 
-        groupSearch = browser.find_element(By.XPATH, "//*[@id='requestForm']/center[1]/fieldset[1]/table/tbody/tr[2]/td[2]/input[1]")
+        groupSearch = browser.find_element(
+            By.XPATH, "//*[@id='requestForm']/center[1]/fieldset[1]/table/tbody/tr[2]/td[2]/input[1]")
         actions.click(groupSearch).perform()
 
         try:
-            elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, "//button[@id='submitBtn']")))
+            elemReady = WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.XPATH, "//button[@id='submitBtn']")))
             print("Select Groups Popup is ready!")
         except TimeoutException:
             print("Loading took too much time!")
 
-        groupSearchFindBtn = browser.find_element(By.XPATH, "//button[@id='submitBtn']")
+        groupSearchFindBtn = browser.find_element(
+            By.XPATH, "//button[@id='submitBtn']")
         actions.click(groupSearchFindBtn).perform()
 
         try:
-            elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "datagrid")))
+            elemReady = WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "datagrid")))
             print("Select Groups List is ready!")
         except TimeoutException:
             print("Loading took too much time!")
 
         for x in range(1, i):
             try:
-                elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@class, "next")]')))
+                elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located(
+                    (By.XPATH, '//button[contains(@class, "next")]')))
                 print("Select Groups List Next Page Button is ready!")
             except TimeoutException:
                 print("Loading took too much time!")
-            
+
             groupSearchSelectManager = browser.find_element(By.ID, "nextPage")
             actions.click(groupSearchSelectManager).perform()
 
@@ -221,47 +282,54 @@ def runFetch():
                 print("Loading took too much time!")'''
 
         try:
-            elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, "selectManager")))
+            elemReady = WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.NAME, "selectManager")))
             print("Select Groups List Next Page Button is ready!")
         except TimeoutException:
             print("Loading took too much time!")
-        
+
         time.sleep(1)
-        
-        groupSearchSelectManager = browser.find_element(By.NAME, "selectManager")
+
+        groupSearchSelectManager = browser.find_element(
+            By.NAME, "selectManager")
         actions.click(groupSearchSelectManager).perform()
 
-        groupSearchSelectForward = browser.find_element(By.XPATH, "//*[@id='formCommands']/tbody/tr/td/span/span")
+        groupSearchSelectForward = browser.find_element(
+            By.XPATH, "//*[@id='formCommands']/tbody/tr/td/span/span")
         actions.click(groupSearchSelectForward).perform()
 
         try:
-            elemReady = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="group"]/option')))
+            elemReady = WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="group"]/option')))
             print("Select Groups Send Result is ready!")
         except TimeoutException:
             print("Loading took too much time!")
 
         browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        groupSearchSubmitButton = browser.find_element(By.XPATH, "//span[contains(@class, 'gradiantButton')]")
+        groupSearchSubmitButton = browser.find_element(
+            By.XPATH, "//span[contains(@class, 'gradiantButton')]")
         actions.click(groupSearchSubmitButton).perform()
 
         print("Start Of Report Generation!")
 
         try:
-            myElem = WebDriverWait(browser, 600).until(EC.presence_of_element_located((By.ID, 'excelBtn')))
+            myElem = WebDriverWait(browser, 600).until(
+                EC.presence_of_element_located((By.ID, 'excelBtn')))
             print("Excel is ready!")
         except TimeoutException:
             print("Loading took too much time!")
 
         time.sleep(3)
 
-        #browser.get_screenshot_as_file(PATH + '/' + 'test.png')
+        # browser.get_screenshot_as_file(PATH + '/' + 'test.png')
 
         excelBtn = browser.find_element(By.ID, "excelBtn")
         actions.click(excelBtn).perform()
 
         excelFilePathElem = browser.find_element(By.NAME, "hd")
         excelFilePath = excelFilePathElem.get_attribute("value")
-        excelFileName = '_' + os.path.splitext(ntpath.basename(excelFilePath))[0] + '.csv'
+        excelFileName = '_' + \
+            os.path.splitext(ntpath.basename(excelFilePath))[0] + '.csv'
 
         while not os.path.exists(os.path.join(PATH, excelFileName)):
             time.sleep(3)
@@ -278,68 +346,85 @@ def runFetch():
 
         shutil.move(lastfile, os.path.join(PATH, str(i) + ".csv"))
 
-        #obtain parent window handle
+        # obtain parent window handle
         p = browser.window_handles[0]
-        #obtain browser tab window
+        # obtain browser tab window
         c = browser.window_handles[1]
-        #switch to tab browser
+        # switch to tab browser
         browser.switch_to.window(c)
-        #close browser tab window
+        # close browser tab window
         browser.close()
-        #switch to parent window
+        # switch to parent window
         browser.switch_to.window(p)
 
-        browser.switch_to.frame(browser.find_elements(By.TAG_NAME, "iframe")[0])
+        browser.switch_to.frame(
+            browser.find_elements(By.TAG_NAME, "iframe")[0])
 
     db = sqlite3.connect('amoozeshyar.db')
     cursor = db.cursor()
-    cursor.execute("UPDATE process_status SET fetchStatus = 'False' WHERE id = 1")
+    cursor.execute(
+        "UPDATE process_status SET fetchStatus = 'False' WHERE id = 1")
     db.commit()
     db.close()
     browser.quit()
 
+    return True
+
+
 @app.get("/result")
-def main(name = None):
+def main(name=None):
     db = sqlite3.connect('amoozeshyar.db')
     processStatus = db.execute("SELECT * from process_status").fetchone()
     db.close()
 
-    if processStatus[1] == 'False':
-        # get list of files that matches pattern
-        pattern = os.path.join(PATH, '*.csv')
-        files = list(filter(checkIsCsv, glob.glob(pattern)))
+    if processStatus[2] == 'False':
+        if processStatus[1] == 'False':
+            # get list of files that matches pattern
+            pattern = os.path.join(PATH, '*.csv')
+            files = list(filter(checkIsCsv, glob.glob(pattern)))
 
-        if len(files) == 4:
-            result = {
-                'status': 'ok',
-                'links': []
-            }
+            if len(files) == 4:
+                result = {
+                    'status': 'ok',
+                    'links': []
+                }
 
-            for filename in files:
-                    result['links'].append(config[4] + '/dl/?name=' + os.path.basename(filename))
+                for filename in files:
+                    result['links'].append(
+                        config[4] + '/dl/?name=' + os.path.basename(filename))
+            else:
+                result = {
+                    'status': 'nok'
+                }
+
+            return result
         else:
             result = {
-                'status': 'nok'
+                'status': 'fetching'
             }
 
-        return result
+            return result
     else:
         result = {
-            'status': 'fetching'
+            'status': 'error'
         }
 
         return result
+
+
 @app.get("/dl/")
 async def read_item(name):
     return FileResponse(path='files/'+name, filename=name, media_type='text/csv')
 
+
 @app.get("/clean-files")
 def cleanFiles():
     cleanDir("files")
-    
+
     db = sqlite3.connect('amoozeshyar.db')
     cursor = db.cursor()
-    cursor.execute("UPDATE process_status SET fetchStatus = 'False' WHERE id = 1")
+    cursor.execute(
+        "UPDATE process_status SET fetchStatus = 'False', fetchError = 'False' WHERE id = 1")
     db.commit()
     db.close()
 
